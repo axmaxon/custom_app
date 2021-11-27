aeaba62607ac1b561e87074a561188d936b3bb74
maxim <axmaxon@gmail.com>
2021-11-27 20:36:28 +0500

    test: Event model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:starttime) }
+    it { should validate_presence_of(:location) }
+    it { should validate_presence_of(:organizermail) }
+  end
 end
