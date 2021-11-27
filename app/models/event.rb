1ee618f56b083e7cd41dd0a55381940d9d6f53a0
maxim <axmaxon@gmail.com>
2021-11-27 12:10:23 +0500

    add: validations to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+  validates :title, presence: true
+  validates :starttime, presence: true
+  validates :location, presence: true
+  validates :organizermail, presence: true
 end
